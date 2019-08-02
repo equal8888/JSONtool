1a4aeef64e8a3e97b9b82406cf086f992aeeeb33
equal8888 <kimi.savander@gmail.com>
2019-08-03 01:51:08 +0300

    faster load time

--- app1.py
+++ app1.py
@@ -9,23 +9,14 @@ app.config['BASIC_AUTH_PASSWORD'] = 'admin'
 
 basic_auth = BasicAuth(app)
 
-database = {
-    0: 'build the codez',
-    1: '<iframe width="100%" height="415" src="https://www.youtube.com/embed/HqrfDNHnevU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-}
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/anotherpage',methods=['POST'])
-def anotherpage():
-    return render_template('anotherpage.html')
-
 @app.route('/json', methods=['GET'])
 #@basic_auth.required
 def json():
-   ReturnDB = database[1].encode('utf-8')
+   ReturnDB = '<iframe width="100%" height="415" src="https://www.youtube.com/embed/HqrfDNHnevU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'.encode('utf-8')
    base64_Get = base64.b64encode(ReturnDB)
    return base64_Get
  #  return ReturnDB

7d3d0977a883c8b6d01aa687037ea8036555e84b
equal8888 <kimi.savander@gmail.com>
2019-09-24 03:36:45 +0300

    update

--- app1.py
+++ app1.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-from flask import Flask, request, render_template, json, g
-#from flask_basicauth import BasicAuth
+from flask import Flask, request, render_template
 import base64
-import os
 
 app = Flask(__name__)
 
@@ -17,12 +15,5 @@ def Gjson01():
     base64_Get01 = base64.b64encode(language)
     return base64_Get01
 
-@app.route('/Gjson03', methods=['GET'])
-def Gjson03():
-    os.system('sudo poweroff')
-    return 'ok'
-
 if __name__ == '__main__':
-#    app.config['BASIC_AUTH_USERNAME'] = 'admin'
-#    app.config['BASIC_AUTH_PASSWORD'] = 'admin'
     app.run(debug=True, port=8888, host='0.0.0.0')

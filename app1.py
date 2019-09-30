8fa5f29da3e44ff54a37adb1d81c280a562f34aa
equal8888 <kimi.savander@gmail.com>
2019-09-30 05:04:21 +0300

    added comments

--- app1.py
+++ app1.py
@@ -6,17 +6,18 @@ import base64
 app = Flask(__name__)
 basic_auth = BasicAuth(app)
 
+# Main page
 @app.route('/')
 def static1():
     return render_template('jsonpagestatic.html')
 
+# Received jdata is converted to base64 and send back to sender
 @app.route('/Gjson01', methods=['POST'])
 @basic_auth.required
 def Gjson01():
     req_data = request.get_json()
     BytestoString = req_data['jdata'].encode('utf-8')
     EncodeBase64 = base64.b64encode(BytestoString)
-
     return EncodeBase64
 
 if __name__ == '__main__':

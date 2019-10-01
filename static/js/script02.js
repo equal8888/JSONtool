d07892d22d9559342ea0ce23746e72c617864f01
equal8888 <kimi.savander@gmail.com>
2019-10-01 08:00:15 +0300

    simplify jsonstringify

--- static/js/script02.js
+++ static/js/script02.js
@@ -59,42 +59,16 @@ function BClear01(elementID) {
 function ReloadPage() {
   location.reload(true);
 };
-// ---
+// Conversion (client-side) JSON.stringify(data)
 const isValidElement = element => {
   return element.name && element.value;
 };
-const isValidValue = element => {
-  return (!['checkbox', 'radio'].includes(element.type) || element.checked);
-};
-const isCheckbox = element => element.type === 'checkbox';
-const isMultiSelect = element => element.options && element.multiple;
-const getSelectValues = options => [].reduce.call(options, (values, option) => {
-  return option.selected ? values.concat(option.value) : values;
-}, []);
-const formToJSON_deconstructed = elements => {
-  const reducerFunction = (data, element) => {
-    data[element.name] = element.value;
-    console.log(JSON.stringify(data));
-    return data;
-  };
-  const reducerInitialValue = {};
-  console.log('Initial `data` value:', JSON.stringify(reducerInitialValue));
-  const formData = [].reduce.call(elements, reducerFunction, reducerInitialValue);
-  return formData;
-};
 const formToJSON = elements => [].reduce.call(elements, (data, element) => {
-  if (isValidElement(element) && isValidValue(element)) {
-    if (isCheckbox(element)) {
-      data[element.name] = (data[element.name] || []).concat(element.value);
-    } else if (isMultiSelect(element)) {
-      data[element.name] = getSelectValues(element);
-    } else {
-      data[element.name] = element.value;
-    };
+  if (isValidElement(element)) {
+    data[element.name] = element.value;
   }
   return data;
 }, {});
-// Show raw data on page
 const handleFormSubmit = event => {
   event.preventDefault();
   const data = formToJSON(form.elements);
@@ -110,7 +84,7 @@ const handleFormSubmit = event => {
     document.getElementById('DisableB01').disabled = false;
   };
 };
-// ---
+// EventListener('submit'
 const form = document.getElementsByClassName('contact-form')[0];
 form.addEventListener('submit', handleFormSubmit);
 // Press Enter to Preview

ea68afbed6a64a52a1d157cf6af780a0ca4b0b86
equal8888 <kimi.savander@gmail.com>
2020-05-16 14:05:58 +0000

    second header

--- static/js/script02.js
+++ static/js/script02.js
@@ -8,34 +8,95 @@ function reloadP() {
 // HOST
   document.getElementById('ShowMyHost').innerHTML = `Status: <span style='color:gray; font-weight: bold;'>n/a</span> <br> Host: <span style='color:gray; font-weight: bold;'>n/a</span>`;
   document.getElementById('urlEntry').value = `http://${location.hostname}:8888/json01`;
-  // HeaderEntry
-  document.getElementById('HeaderEntry').value = 'Authorization';
+
   // Auth
-  document.getElementById('authEntry').value = 'Basic [YWRtaW46YWRtaW4=]';
+  document.getElementById('authEntry01').value = 'application/json;charset=UTF-8';
+  document.getElementById('authEntry02').value = 'Basic [YWRtaW46YWRtaW4=]';
   // Method
   document.getElementById('methodEntry').value = 'GET';
   // DATA
   document.getElementById("valueEntry").value = 'a';
   // VALUE
   document.getElementById('valueId').value = '<iframe width="100%" height="400" src="https://www.youtube.com/embed/eMDQfSrpLlQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-};
 
+  // HeaderEntry01data
+  var headerlist = `
+            <option value="">---</option>
+            <option value="Accept">Accept</option>
+            <option value="Accept-Charset">Accept-Charset</option>
+            <option value="Accept-Encoding">Accept-Encoding</option>
+            <option value="Accept-Language">Accept-Language</option>
+            <option value="Access-Control-Request-Headers">Access-Control-Request-Headers</option>
+            <option value="Access-Control-Request-Method">Access-Control-Request-Method</option>
+            <option value="Authorization">Authorization</option>
+            <option value="Cache-Control">Cache-Control</option>
+            <option value="Connection">Connection</option>
+            <option value="Content-Language">Content-Language</option>
+            <option value="Content-Type">Content-Type</option>
+            <option value="Cookie">Cookie</option>
+            <option value="Date">Date</option>
+            <option value="DNT">DNT</option>
+            <option value="DPR">DPR</option>
+            <option value="Early-Data">Early-Data</option>
+            <option value="Expect">Expect</option>
+            <option value="Forwarded">Forwarded</option>
+            <option value="From">From</option>
+            <option value="Host">Host</option>
+            <option value="If-Match">If-Match</option>
+            <option value="If-Modified-Since">If-Modified-Since</option>
+            <option value="If-None-Match">If-None-Match</option>
+            <option value="If-Range">If-Range</option>
+            <option value="If-Unmodified-Since">If-Unmodified-Since</option>
+            <option value="Keep-Alive">Keep-Alive</option>
+            <option value="Max-Forwards">Max-Forwards</option>
+            <option value="Origin">Origin</option>
+            <option value="Pragma">Pragma</option>
+            <option value="Proxy-Authorization">Proxy-Authorization</option>
+            <option value="Range">Range</option>
+            <option value="Referer">Referer</option>
+            <option value="Save-Data">Save-Data</option>
+            <option value="TE">TE</option>
+            <option value="Trailer">Trailer</option>
+            <option value="Transfer-Encoding">Transfer-Encoding</option>
+            <option value="Upgrade">Upgrade</option>
+            <option value="Upgrade-Insecure-Requests">Upgrade-Insecure-Requests</option>
+            <option value="User-Agent">User-Agent</option>
+            <option value="Vary">Vary</option>
+            <option value="Via">OPTIONS</option>
+            <option value="Viewport-Width">Viewport-Width</option>
+            <option value="Warning">Warning</option>
+            <option value="Width">Width</option>
+            `;
+
+            // HeaderEntry02 value
+
+            document.getElementById('HeaderEntry01').innerHTML = headerlist
+            document.getElementById('HeaderEntry02').innerHTML = headerlist
+
+
+            document.getElementById('HeaderEntry01').value = 'Content-Type';
+            document.getElementById('HeaderEntry02').value = 'Authorization';
+
+
+}
 // memo
 //   crossOrigin: true,
 //  dataType: "jsonp",
 
-function loadJSON(data22,method,pageurl,newheadr,newauth) {
+function loadJSON(data22,method,pageurl,he01,heval01,he02,heval02) {
 
   const xhr = new XMLHttpRequest();
 
   var params = `${data22}`;
   var host  = (`${pageurl}`);
-  var newheadr = `${newheadr}`;
-  var newauth = (`${newauth}`);
+  var he01 = `${he01}`;
+  var heval01 = (`${heval01}`);
+  var he02 = `${he02}`;
+  var heval02 = (`${heval02}`);
 
   xhr.open(`${method}`, `${host}`);
-  xhr.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
-  xhr.setRequestHeader(`${newheadr}`, `${newauth}`);
+  xhr.setRequestHeader(`${he01}`, `${heval01}`);
+  xhr.setRequestHeader(`${he02}`, `${heval02}`);
   xhr.onreadystatechange = function() {
   xhr.timeout = 2000;
 
@@ -95,12 +156,18 @@ function BPreview01() {
   var nameElement04 = document.getElementById("methodEntry");
   var method = nameElement04.value;
 // HEADERS
-  var name05 = document.getElementById("HeaderEntry");
-  var newheadr = name05.value;
+  var name06 = document.getElementById("HeaderEntry01");
+  var he01 = name06.value;
+
+  var name05 = document.getElementById("HeaderEntry02");
+  var he02 = name05.value;
 
 // AUTH
-  var name03 = document.getElementById("authEntry");
-  var newauth = name03.value;
+  var name07 = document.getElementById("authEntry01");
+  var heval01 = name07.value;
+
+  var name03 = document.getElementById("authEntry02");
+  var heval02 = name03.value;
 
 // DATA
   var name02 = document.getElementById("valueEntry");
@@ -109,7 +176,7 @@ function BPreview01() {
 // VALUE
   var valuename = document.getElementById("valueId").name = newname;
 
-    loadJSON(`${data22}`,`${method}`,`${jhost}`,`${newheadr}`,`${newauth}`);
+    loadJSON(`${data22}`,`${method}`,`${jhost}`,`${he01}`,`${heval01}`,`${he02}`,`${heval02}`);
 };
 
 
@@ -126,10 +193,12 @@ function ReloadPage() {
 function reset() {
   // Host
   document.getElementById('urlEntry').value = `http://${location.hostname}:8888/json01`;
-  // HeaderEntry
-  document.getElementById('HeaderEntry').value = 'Authorization';
+  // HeaderEntry02
+  document.getElementById('HeaderEntry01').value = 'Content-Type';
+  document.getElementById('HeaderEntry02').value = 'Authorization';
   // Auth
-  document.getElementById('authEntry').value = 'Basic [YWRtaW46YWRtaW4=]';
+  document.getElementById('authEntry01').value = 'application/json;charset=UTF-8';
+  document.getElementById('authEntry02').value = 'Basic [YWRtaW46YWRtaW4=]';
   // Method
   document.getElementById('methodEntry').value = 'GET';
   // DATA

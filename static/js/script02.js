7aed8ec790cd330dffb5158702a9414eb7fbedb4
equal8888 <kimi.savander@gmail.com>
2020-05-17 15:41:56 +0000

    tweak

--- static/js/script02.js
+++ static/js/script02.js
@@ -260,8 +260,6 @@ function reset() {
   document.getElementById('methodEntry').value = 'POST';
   // DATA
   document.getElementById('valueEntry').value = 'data01';
-  // VALUE
-  document.getElementById('valueId').value = '<iframe width="100%" height="400" src="https://www.youtube.com/embed/eMDQfSrpLlQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 };
 
 

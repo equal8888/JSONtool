8d1114dda102e8b415adb340eecaac12556f1069
equal8888 <kimi.savander@gmail.com>
2020-05-21 18:32:54 +0000

    1

--- static/js/script02.js
+++ static/js/script02.js
@@ -154,7 +154,7 @@ function testv() {
   // Method
   document.getElementById('methodEntry').value = 'POST';
   // VALUE
-  document.getElementById('valueId01').value = '<iframe width="100%" height="500" src="https://www.youtube.com/embed/P1ww1IXRfTA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+  document.getElementById('valueId01').value = '<iframe width="100%" height="500" src="https://www.youtube.com/embed/eMDQfSrpLlQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 };
 function reset() {
 

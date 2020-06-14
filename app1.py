a43cda1c6421d3b099f9528e726ecb707ba4caa0
equal8888 <kimi.savander@gmail.com>
2020-06-14 04:06:50 +0300

    flask static folder setup

--- app1.py
+++ app1.py
@@ -3,7 +3,7 @@ from flask import Flask, request, render_template
 from flask_basicauth import BasicAuth
 import json
 
-app = Flask(__name__)
+app = Flask(__name__ ,static_url_path='/static')
 basic_auth = BasicAuth(app)
 
 # Main page

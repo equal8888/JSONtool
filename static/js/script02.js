ed3847696bd9e707a09654cef716498355ab8b75
equal8888 <30691848+equal8888@users.noreply.github.com>
2020-10-20 02:29:25 +0300

    Update script02.js

--- static/js/script02.js
+++ static/js/script02.js
@@ -652,7 +652,7 @@ function testv() {
         "genre":"Fantasy Fiction"
   },
   "videos":{
-    "music01":"<iframe width="100%" height="400" src="https://www.youtube.com/embed/jEw1y91FzYw?start=570" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>"
+    "music01":"<iframe width='100%' height='400' src='https://www.youtube.com/embed/jEw1y91FzYw?start=570' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
   }
   }`;
 };
